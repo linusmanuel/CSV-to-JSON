3abbf1b0a5d1f53ade44c1d453f73e8e36bfda1f
Nicasiomarques <nicasiomarques18@gmail.com>
2022-09-11 20:26:17 +0100

    refactor: improve cleat field method legibility

--- resources/app.js
+++ resources/app.js
@@ -1,63 +1,57 @@
 const $ = document.querySelector.bind(document);
 
-const btnCSV = $('.btn--csv');
-const btnJSON = $('.btn--json');
-const btnClear = $('.btn--clear');
+const btnCSV = $(".btn--csv");
+const btnJSON = $(".btn--json");
+const btnClear = $(".btn--clear");
 
-const inputCSV = $('.input--csv');
-const inputJson = $('.input--json');
+const inputCSV = $(".input--csv");
+const inputJson = $(".input--json");
 
-class RequiredFieldError extends Error { 
-  constructor(fieldName = '') { 
-    super(`O campo ${fieldName} e obrigatorio!`)
-    this.name = 'RequiredFieldError'
+class RequiredFieldError extends Error {
+  constructor(fieldName = "") {
+    super(`O campo ${fieldName} e obrigatorio!`);
+    this.name = "RequiredFieldError";
   }
 }
 
-const inputIsEmpty = value => 
-  !value ? new RequiredFieldError() : null
+const inputIsEmpty = value => (!value ? new RequiredFieldError() : null);
 
-const validateCsvToJson = (
-  value = '', 
-  validators = [inputIsEmpty]
-) => {
+const validateCsvToJson = (value = "", validators = [inputIsEmpty]) => {
   for (const validator of validators) {
-    const error = validator(value) 
-    if(error) return error.message
+    const error = validator(value);
+    if (error) return error.message;
   }
-}
-
-const convertJsonToCsv = (csvText, tabSize = 2) => {
-  const { headers, rows } = getHeadersAndRows(csvText);
-  const parsedJson = rows.reduce(
-    (arr, row) => [
-      ...arr,
-      row
-        .split(',')
-        .reduce((obj, attr, index) => ({ ...obj, [headers[index]]: attr }), {}),
-    ],
-    []
-  );
-  return JSON.stringify(parsedJson, null, tabSize);
 };
 
-const getHeadersAndRows = (csvText, separator = ',') => {
-  const [firstRow, ...rows] = csvText.trim().replace(/"/g, '').split('\n');
+const getHeadersAndRows = (csvText, separator = ",") => {
+  const [firstRow, ...rows] = csvText.trim().replace(/"/g, "").split("\n");
   return {
     headers: firstRow.split(separator),
     rows,
   };
 };
 
-btnClear.onclick = () => ((fieldToClear = [inputCSV, inputJson]) => {
-  fieldToClear.forEach(field => field.value = '')
-})()
+const convertJsonToCsv = (csvText, tabSize = 2) => {
+  const { headers, rows } = getHeadersAndRows(csvText);
+  const parsedJson = rows.reduce(
+    (arr, row) => [
+      ...arr,
+      row.split(",").reduce((obj, attr, index) => ({ ...obj, [headers[index]]: attr }), {}),
+    ],
+    []
+  );
+  return JSON.stringify(parsedJson, null, tabSize);
+};
+
+const clearControls = (inputs = []) => inputs.forEach(input => (input.value = ""));
+
+btnClear.onclick = () => clearControls([inputCSV, inputJson]);
 
 btnCSV.onclick = () => {
-  const validationError = validateCsvToJson(inputCSV.value)
+  const validationError = validateCsvToJson(inputCSV.value);
   if (validationError) {
-    alert(validationError)
-    return
+    alert(validationError);
+    return;
   }
   inputJson.value = convertJsonToCsv(inputCSV.value);
-}
+};
